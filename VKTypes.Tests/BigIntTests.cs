eaab2d09f44a2ecaa2a8e341c4a2be4f108e2dda
valercheck <mypowerismind@gmail.com>
2016-10-01 01:25:03 +0300

    Add FromString conversion

--- VKTypes.Tests/BigIntTests.cs
+++ VKTypes.Tests/BigIntTests.cs
@@ -13,7 +13,28 @@ namespace VKTypes.Tests
             int testInt = 895302938;
             BigInt testBigInt = new BigInt(testInt);
             Assert.AreEqual(testInt.ToString(), testBigInt.ToString());
-
         }
+
+        [Test]
+        public void FromStringToByteConversionSuccessTest()
+        {
+            int testInt = 895302938;
+            BigInt testBigInt = new BigInt(testInt.ToString());
+            Assert.AreEqual(testInt.ToString(), testBigInt.ToString());
+        }
+
+        [Test]
+        public void CanParseStringToBigInt()
+        {
+            string notDigitInTheMiddle = "12331f123123";
+            string notDigitAtStart = "+af3234234234";
+            string notDigitAtEnd = "12312312../";
+            string validString = "12345234234456457456";
+
+            Assert.IsFalse(BigInt.CanParse(notDigitInTheMiddle));
+            Assert.IsFalse(BigInt.CanParse(notDigitAtStart));
+            Assert.IsFalse(BigInt.CanParse(notDigitAtEnd));
+            Assert.IsTrue(BigInt.CanParse(validString));
+        } 
     }
 }
--- VKTypes/BigInt.cs
+++ VKTypes/BigInt.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,9 +24,46 @@ namespace VKTypes
             return _resultBytes;
         }
 
+        private List<byte> FromStringToBytes(string number)
+        {
+            List<byte> _result = new List<byte>();
+            if (!CanParse(number))
+                throw new ArgumentException("Check your string input, it can contains some not digit characters");
+            foreach (var ch in number) _result.Add(Convert.ToByte(ch.ToString()));
+            return _result;
+        }
+
+        /*private BigInt Multiply(BigInt another)
+        {
+            List<byte> result = new List<byte>();
+            //return new BigInt(result);
+
+        }*/
+
+        public static bool CanParse(string number)
+        {
+            if (Regex.IsMatch(number, @"[^\d]")) return false;
+            return true;
+        }
+
+        /*public static BigInt ValueOf(string number)
+        {
+
+        }*/
+
         public BigInt(int number)
         {
             _digits = FromIntToBytes(number);
+        }
+
+        public BigInt(string number)
+        {
+            _digits = FromStringToBytes(number);
+        }
+
+        public static BigInt Zero()
+        {
+            return new BigInt(0);
         }
 
         public override string ToString()
