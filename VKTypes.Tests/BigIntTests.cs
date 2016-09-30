209912e821553d0e59c32db855e0b8b537be60a4
valercheck <mypowerismind@gmail.com>
2016-10-01 00:10:01 +0300

    Add Tests project
    Add FromInt conversion method

---
+++ VKTypes.Tests/BigIntTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using VKTypes;
+
+namespace VKTypes.Tests
+{
+    [TestFixture]
+    public class BigIntTests
+    {
+        [Test]
+        public void FromIntToByteConversionSuccessTest()
+        {
+            int testInt = 895302938;
+            BigInt testBigInt = new BigInt(testInt);
+            Assert.AreEqual(testInt.ToString(), testBigInt.ToString());
+
+        }
+    }
+}
---
+++ VKTypes/BigInt.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKTypes
+{
+    public class BigInt
+    {
+        private List<byte> _digits = new List<byte>();
+
+        private List<byte> FromIntToBytes(int number)
+        {
+            List<byte> _resultBytes = new List<byte>();
+            int digit = 0;
+            while (number > 0)
+            {
+                digit = number % 10;
+                _resultBytes.Insert(0, Convert.ToByte(digit));
+                number /= 10;
+            }
+            return _resultBytes;
+        }
+
+        public BigInt(int number)
+        {
+            _digits = FromIntToBytes(number);
+        }
+
+        public override string ToString()
+        {
+            return _digits.Aggregate("",(result, digit) => result += digit);
+        }
+    }
+}
